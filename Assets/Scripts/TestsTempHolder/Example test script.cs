2d19036ef58dea81a311544a6f94b8156dff307f
jonscott2 <jonathan.scott6@snhu.edu>
2025-04-03 14:27:14 -0400

    Completed example test
    Example test now shows how to create a player in the test and get a value, health, from that newly created player

--- Assets/Scripts/TestsTempHolder/Example test script.cs
+++ Assets/Scripts/TestsTempHolder/Example test script.cs
@@ -9,14 +9,12 @@ public class Exampletestscript
     // A Test behaves as an ordinary method
     //setting up. Must be up here
     //creates a playerPrefab object to test
+
     public static GameObject playerPrefab = Resources.Load<GameObject>("Player");
 
-    //public static Vector3 playerPos = Vector3.zero;
-    //public static Quaternion playerDir = Quaternion.identity;
     public static GameObject player = GameObject.Instantiate(playerPrefab);
 
-    //public Unit unitscript;
-    //Unit unitRef = instance.GetComponent<Unit>();
+    int health2 = player.GetComponent<Unit>().currentHP;
 
     [Test]
     
@@ -27,14 +25,8 @@ public class Exampletestscript
     //checks that player game object is running correctly.
     Assert.IsNotNull(player, "Player is not working");
 
-        // Use the Assert class to test conditions
-        //what is actually being tested
-
-    //Unit thisthing = new GameObject().AddComponent<Unit>();
-    //Assert.That(player.GetComponent<Unit>.currentHP, Is.EqualTo(25f));
+    Assert.That(health2, Is.EqualTo(25f));
     }
-
-    //Unit x = player.GetComponent<Unit>.currentHP
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
